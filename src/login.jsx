c82da442864ea28fb93a7ed6b1758fb8564aa47d
sharat <k.g.sharataa124@gmail.com>
2023-04-22 03:15:27 +0530

    changed

--- src/login.jsx
+++ src/login.jsx
@@ -2,8 +2,8 @@ export const Login = () => {
     return(
                 <form>
            
-                <input type="text" name="username" placeholder="Username" required/>
-                 <input type="password" name="password" placeholder="Password" required/>
+                <input type="text" name="username" placeholder="Username" required/><br></br>
+                 <input type="password" name="password" placeholder="Password" required/><br></br>
                 <input type="submit" value="Login"/>
             
                 </form>
