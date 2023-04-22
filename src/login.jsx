e4867e8dcf1b93d056f6330794687d9cb877aa44
sharat <k.g.sharataa124@gmail.com>
2023-04-22 11:11:55 +0530

    created

--- src/login.jsx
+++ src/login.jsx
@@ -1,9 +1,13 @@
+import React, { useState } from "react";
+
 export const Login = () => {
+    const [email, setEmaill] = useState('');
+    const [pass, setPass] = useState('');
     return(
                 <form>
            
                 <input type="text" name="username" placeholder="Username" required/><br></br>
-                 <input type="password" name="password" placeholder="Password" required/><br></br>
+                 <input type="password" name="password" placeholder="Password" required/><br>f</br>
                 <input type="submit" value="Login"/>
             
                 </form>
