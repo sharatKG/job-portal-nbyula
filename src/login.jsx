3df44c7ae365066d785e42936c829febbc987ea5
sharat <k.g.sharataa124@gmail.com>
2023-04-22 16:31:49 +0530

    changed

--- src/login.jsx
+++ src/login.jsx
@@ -11,14 +11,15 @@ export const Login = (props) => {
     }
     return(
         <div className = "auth-form-container">
-        <form onSubmit={handleSubmit}>
+            <h2>Login</h2>
+        <form className="login-form" onSubmit={handleSubmit}>
              <label htmlfor="email">email</label>
              <input  onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="youremail@gmail.com" required/><br></br>
              <label htmlfor="password">password</label>
                 <input onChange={(e) => setPass(e.target.value)} type="password" name="password" placeholder="********" id="password" required/><br></br>
                 <button type="submit">Log In</button>
             </form>
-            <button onClick={()=>props.onFormSwitch('signup')}>Don't have an account? Register here.</button>
+            <button className="link-btn"onClick={()=>props.onFormSwitch('signup')}>Don't have an account? Register here.</button>
            </div> 
         )
 
--- src/signup.jsx
+++ src/signup.jsx
@@ -10,16 +10,17 @@ export const Signup = (props) =>{
     }
     return(
         <div className = "auth-form-container">
-        <form onSubmit={handleSubmit}>
+            <h2>Sign Up</h2>
+        <form className="signup-form" onSubmit={handleSubmit}>
             <label htmlfor="name">fullname</label>
             <input value={name} name="name" id="name" placeholder="fullname" />
-             <label htmlfor="email">ema</label>
+             <label htmlfor="email">email</label>
              <input  onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="youremail@gmail.com" required/><br></br>
              <label htmlfor="password">password</label>
                 <input onChange={(e) => setPass(e.target.value)} type="password" name="password" placeholder="********" id="password" required/><br></br>
                 <button type="submit">Log In</button>
             </form>
-            <button onClick={()=>props.onFormSwitch('Login')}>Already have an account? Login here.</button>
+            <button className="link-btn" onClick={()=>props.onFormSwitch('login')}>Already have an account? Login here.</button>
            </div> 
     )
 
