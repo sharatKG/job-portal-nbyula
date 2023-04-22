89be966806af865f813795beab628545c7b4713f
sharat <k.g.sharataa124@gmail.com>
2023-04-22 12:42:35 +0530

    created

--- src/login.jsx
+++ src/login.jsx
@@ -1,16 +1,25 @@
 import React, { useState } from "react";
-
 export const Login = () => {
-    const [email, setEmaill] = useState('');
+    const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email);
+
+    }
     return(
-                <form>
-           
-                <input type="text" name="username" placeholder="Username" required/><br></br>
-                 <input type="password" name="password" placeholder="Password" required/><br>f</br>
-                <input type="submit" value="Login"/>
-            
-                </form>
-    )
+        <>
+        <form onSubmit={handleSubmit}>
+             <label for="email">email</label>
+             <input  onChange={(e) => setEmail(e.target.value)} type="email" name="email" placeholder="youremail@gmail.com" required/><br></br>
+             <label for="password">password</label>
+                <input onChange={(e) => setPass(e.target.value)} type="password" name="password" placeholder="********" id="password" required/><br></br>
+                <button type="submit">Log In</button>
+            </form>
+            <button>Don't have an account? Register here.</button>
+           </> 
+        )
 
 }
