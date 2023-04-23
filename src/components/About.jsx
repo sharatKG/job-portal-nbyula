7a740911b6d9d071ca5e4e10559dfc26cbc95a00
sharat <k.g.sharataa124@gmail.com>
2023-04-23 16:06:18 +0530

    created

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,25 +1,12 @@
 import React, { useState } from "react";
 import navbar from './Navbar';
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
-
 const About = () => {
-  return (
+    return( 
+    <div>
+        <navbar />
     
-  )
-}
-
-function CustomLink({ to, children, ...props }) {
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-
-  return (
-    <li className={isActive ? "active" : ""}>
-      <Link to={to} {...props}>
-        {children}
-      </Link>
-    </li>
-  )
+    </div>
+    );  
 };
-
 
 export default About;
